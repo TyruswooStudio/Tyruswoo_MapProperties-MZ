d9b6f11ac578c468bd6ad6cd898ffdab16353dcd
Kathy Bunn <mckathlin@gmail.com>
2023-08-29 23:14:45 -0400

    updated copyright owner names

--- Tyruswoo_MapProperties.js
+++ Tyruswoo_MapProperties.js
@@ -7,7 +7,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2023 Tyruswoo Team
+ * Copyright (c) 2023 Kathy Bunn and Scott Tyrus Washburn
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
